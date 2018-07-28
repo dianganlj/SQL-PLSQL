f92423ec802cf53e934dc248c629469e2f2e0186
diangan <1432581545@qq.com>
2018-07-28 09:06:19 +0800

    Update test.sql

--- lijie/test.sql
+++ lijie/test.sql
@@ -3,4 +3,5 @@ create or replace function lijie_test(p_id in number) return  varchar2 is
 begin 
  select last_name into v_name from employees where employee_id = p_id ;
  return v_name;
+ --这个是一个注释，用于检测文件同步信息
  end;
